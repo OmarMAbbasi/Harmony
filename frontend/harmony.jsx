1e6be5ace6c9b7ed15dc859e6717e9109081a2fd
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-03 09:40:42 -0700

    Root working

--- frontend/harmony.jsx
+++ frontend/harmony.jsx
@@ -1,1 +1,9 @@
-<h1>React is working</h1>
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+	// const store = configureStore();
+	const root = document.getElementById("root");
+	// ReactDOM.render(<Root store={store} />, root);
+	ReactDOM.render(<h1>ReactIsWorking</h1>);
+});
