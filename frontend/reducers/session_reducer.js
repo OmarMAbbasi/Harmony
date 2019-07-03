2417c078b3f70dbe860696db31754a1ea7ed7c9a
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-03 15:42:13 -0700

    Build users and session reducers

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,27 @@
+import {
+	RECEIVE_CURRENT_USER,
+	LOGOUT_CURRENT_USER,
+	RECEIVE_ERRORS
+} from "../actions/session_actions";
+
+const _nullUser = {
+    id: null
+}
+
+const sessionReducer = (oldState = _nullUser, action) => {
+	Object.freeze(oldState);
+	let newState = Object.assign({}, oldState);
+	switch (action.type) {
+		case RECEIVE_CURRENT_USER:
+            newState[id] = action.currentUser.id
+			return newState;
+		case LOGOUT_CURRENT_USER:
+			return newState;
+		case RECEIVE_ERRORS:
+			return newState;
+		default:
+			return oldState;
+	}
+};
+
+export default sessionReducer;
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,15 @@
+import { RECIEVE_CURRENT_USER } from "action_path";
+
+const someReducer = (oldState = {}, action) => {
+	Object.freeze(oldState);
+	let newState = Object.assign({}, oldState);
+	switch (action.type) {
+		case RECIEVE_CURRENT_USER:
+			newState[action.user.id] = action.user;
+			return newState;
+		default:
+			return oldState;
+	}
+};
+
+export default someReducer;
