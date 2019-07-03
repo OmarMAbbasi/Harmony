229ae42dcfc196e05efdf144781c157e2b88e6cb
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-03 09:48:49 -0700

    Migrate users table

---
+++ db/migrate/20190703164149_create_users.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.integer :digits, null: false
+      t.string :email
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
+    add_index :users, [:username, :digits], unique: true
+    add_index :users, :session_token, unique: true
+    add_index :users, :email, unique:true
+    #Ex:- add_index("admin_users", "username")
+  end
+end
