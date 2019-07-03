4a42a99c327fe9b363ba10382a6082870e96d7e5
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-03 16:04:46 -0700

    Build store

--- frontend/harmony.jsx
+++ frontend/harmony.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import * as SessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
-	// const store = configureStore();
 	const root = document.getElementById("root");
+	ReactDOM.render(<h1>ReactIsWorking</h1>);
+	// const store = configureStore();
 	// ReactDOM.render(<Root store={store} />, root);
+	
+	
+	//Temporary Window Debugger methods
 	window.signup = SessionAPIUtil.signup;
 	window.login = SessionAPIUtil.login;
 	window.logout = SessionAPIUtil.logout;
-
-	ReactDOM.render(<h1>ReactIsWorking</h1>);
+	window.getState = store.getState;
+	window.dispatch = store.dispatch; 
 });
-const
