ea6bae727d035dd23915e87aa97ff94a702d4ed1
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-04 00:29:36 -0700

    Splash logic scaffold complete

--- frontend/components/landing/splash_container.jsx
+++ frontend/components/landing/splash_container.jsx
@@ -19,11 +19,31 @@ const mapDispatchToProps = dispatch => ({
 	signup: () => dispatch(signup())
 });
 
+// function LoginButton(props) {
+// 	return <button onClick={props.onClick}>Login</button>;
+// }
+
+// function LogoutButton(props) {
+// 	return (<button onClick={props.onClick}>Logout</button>;)
+// }
+
+function OpenButton({ openUsernameField, currentUser }) {
+	if (!currentUser) {
+		return <button onClick={openUsernameField}>Open Discord</button>;
+	} else {
+		return <Link to={`/home/`}>Open Discord</Link>;
+	}
+}
+
+//<button>Open Discord</button>
+
 class Splash extends React.Component {
 	constructor(props) {
 		super(props);
 		this.newDummyUser = this.newDummyUser.bind(this);
 		this.demoLogin = this.demoLogin.bind(this);
+		this.openUsernameField = this.openUsernameField.bind(this);
+		this.state = { opened: false };
 	}
 
 	newDummyUser(e) {
@@ -34,23 +54,56 @@ class Splash extends React.Component {
 		e.preventDefault();
 		console.log("Demo Login Script");
 	}
+
+	openUsernameField(e) {
+		e.preventDefault();
+		this.setState({ opened: true });
+		console.log("hi");
+	}
+
+	// render() {
+	// 	const isLoggedIn = this.state.isLoggedIn;
+	// 	let button;
+
+	// 	if (isLoggedIn) {
+	// 		button = <LogoutButton onClick={this.handleLogoutClick} />;
+	// 	} else {
+	// 		button = <LoginButton onClick={this.handleLoginClick} />;
+	// 	}
+
+	// 	return <div>{button}</div>;
+	// }
+
 	render() {
-		return (
-			<div>
-				<h1>Splash Page</h1>
+		const opened = this.state.opened;
+		let splash;
+		if (!opened) {
+			splash = (
 				<ul>
 					<li>
 						<button onClick={this.demoLogin}>Demo Login</button>
 					</li>
+					<OpenButton
+						currentUser={this.currentUser}
+						openUsernameField={this.openUsernameField}
+					/>
+
 					<br></br>
-					<li>
-						<form onSubmit={this.newDummyUser}>
-							<input type="text" placeholder="enter a username"></input>
-							<button onClick={this.newUser}>Arrow</button>
-						</form>
-						<p>Already have an account? </p>
-					</li>
 				</ul>
+			);
+		} else {
+			splash = (
+				<form onSubmit={this.newDummyUser}>
+					<input type="text" placeholder="enter a username"></input>
+					<button onClick={this.newDummyUser}>Arrow</button>
+					<Link to={`/login/`}>Already have an account? </Link>
+				</form>
+			);
+		}
+		return (
+			<div>
+				<h1>Splash Page</h1>
+				{splash}
 			</div>
 		);
 	}
