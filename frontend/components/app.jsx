1aeaa97cfb9f1e0fceb650eb9cb42c574b8edb8e
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-05 23:34:51 -0700

    Fix gitlens leftovers

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,11 +4,6 @@ import { Switch, Router, Route, Link } from "react-router-dom";
 import LandingContainer from "./landing/landing";
 import RegisterContainer from "./auth/register_container";
 import LoginContainer from "./auth/login_container";
-<<<<<<< HEAD
-=======
-import HomeContainer from './home/home_container';
-
->>>>>>> development
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 const App = () => (
@@ -16,10 +11,6 @@ const App = () => (
 		<Switch>
 			<AuthRoute exact path="/login" component={LoginContainer} />
 			<AuthRoute exact path="/register" component={RegisterContainer} />
-<<<<<<< HEAD
-=======
-			<Route exact path="/home/" component={HomeContainer} />
->>>>>>> development
 			<Route exact path="/" component={LandingContainer} />
 		</Switch>
 	</div>
