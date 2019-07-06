ea0e916856a191a98e62b25cb5f5a687cf23744b
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-05 21:27:10 -0700

    Login page fully styled

--- frontend/components/landing/footer/footer_container.jsx
+++ frontend/components/landing/footer/footer_container.jsx
@@ -14,13 +14,13 @@ const Footer = props => {
 					<h2 className="ready-yet">Ready to try Discord? It's free!</h2>
 					<h3 className="other-players">JOIN OVER 250 MILLION PLAYERS TODAY</h3>
 				</div>
-					<Link
-						className="join-button"
-						style={{ color: "white" }}
-						to={`/register/`}
-					>
-						Open Discord
-					</Link>
+				<Link
+					className="join-button"
+					style={{ color: "white" }}
+					to={`/register/`}
+				>
+					Join Discord
+				</Link>
 			</div>
 		</footer>
 	);
