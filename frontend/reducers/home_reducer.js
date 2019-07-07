30c8e10ebfd777d1000d5bf3eaf3b15527e7f8fa
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-06 17:49:41 -0700

    Import and export home reducer

--- frontend/reducers/home_reducer.js
+++ frontend/reducers/home_reducer.js
@@ -1,15 +1,15 @@
-import {
-ACTION_CONSTANTS
-} from 'action_path';
+// import { ACTION_CONSTANTS } from "action_path";
 
-const someReducer = (oldState = {}, action) => {
-  Object.freeze(oldState);
-  let newState = Object.assign({}, oldState);
-  return oldState;
-//   switch(action.type) {
-//     case //SOME_ACTION_CONSTANT	:
-//       return newState;
-//     default:
-//       return oldState;
-//   }
+const homeReducer = (oldState = {}, action) => {
+	Object.freeze(oldState);
+	let newState = Object.assign({}, oldState);
+	return oldState;
+	//   switch(action.type) {
+	//     case //SOME_ACTION_CONSTANT	:
+	//       return newState;
+	//     default:
+	//       return oldState;
+	//   }
 };
+
+export default homeReducer;
