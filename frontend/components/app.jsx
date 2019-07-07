6d74a3365fb29ccab1740911bef674d017aaef47
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-06 18:48:44 -0700

    Nope

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Switch, Router, Route, Link } from "react-router-dom";
 
-import HomeContainer from "./home/home_container";
-import LandingContainer from "./landing/landing_container";
+import LandingContainer from "./landing/landing";
 import RegisterContainer from "./auth/register_container";
 import LoginContainer from "./auth/login_container";
 
@@ -13,7 +12,6 @@ const App = () => (
 		<Switch>
 			<AuthRoute exact path="/login" component={LoginContainer} />
 			<AuthRoute exact path="/register" component={RegisterContainer} />
-			<ProtectedRoute path="/home" component={HomeContainer} />
 			<Route exact path="/" component={LandingContainer} />
 		</Switch>
 	</div>
