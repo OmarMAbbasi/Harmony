a3749714861e56dd748e577a209fff3371854b66
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-08 15:54:57 -0700

    comment for heroku

--- frontend/util/guild_api_util.js
+++ frontend/util/guild_api_util.js
@@ -1,32 +1,32 @@
-export const create = user =>
-	$.ajax({
-		method: "POST",
-		url: "/api/session",
-		data: { user }
-	});
-
-export const update = user =>
-	$.ajax({
-		method: "POST",
-		url: "/api/users",
-		data: { user }
-	});
-
-// export const claim = user =>
+// export const create = user =>
 // 	$.ajax({
-// 		url: `api/user/${user.id}`,
-// 		method: "PATCH",
+// 		method: "POST",
+// 		url: "/api/session",
 // 		data: { user }
 // 	});
 
-// export const abandon = id =>
+// export const update = user =>
 // 	$.ajax({
-// 		url: `api/user/${id}`,
-// 		method: "DELETE"
+// 		method: "POST",
+// 		url: "/api/users",
+// 		data: { user }
 // 	});
 
-export const destroy = () =>
-	$.ajax({
-		method: "DELETE",
-		url: "api/session"
-	});
+// // export const claim = user =>
+// // 	$.ajax({
+// // 		url: `api/user/${user.id}`,
+// // 		method: "PATCH",
+// // 		data: { user }
+// // 	});
+
+// // export const abandon = id =>
+// // 	$.ajax({
+// // 		url: `api/user/${id}`,
+// // 		method: "DELETE"
+// // 	});
+
+// export const destroy = () =>
+// 	$.ajax({
+// 		method: "DELETE",
+// 		url: "api/session"
+// 	});
