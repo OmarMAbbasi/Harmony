811f34aa9f1fb3b648b9eaf201bd898a65315d8d
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-10 14:48:21 -0700

    Channel skeleton

--- frontend/components/homebase/guild/guild.jsx
+++ frontend/components/homebase/guild/guild.jsx
@@ -23,11 +23,32 @@ class Guild extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="guild-box">
 				<div className="guild-bar">
 					<GuildTag guildId={this.props.match.params.guildId} />
 					<ChannelList guildId={this.props.match.params.guildId} />
+
 					<UserAnchor />
+				</div>
+				<div className="channel-box">
+					<div className="channel-header">
+						<div>Channel Title</div>
+					</div>
+					<div className="channel-content">
+						<div className="chat-content">
+							<div className="message-box">Messages</div>
+							<form className="message-form">
+								<div className="message-form-wrapper">
+									<div className="message-form-input-area">
+										<div className="message-inner-form">
+											<textarea className="message-text-area"></textarea>
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div style={{ display: "none" }}>FriendList</div>
+					</div>
 				</div>
 			</div>
 		);
